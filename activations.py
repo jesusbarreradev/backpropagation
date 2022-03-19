a72d1eaf1bec35cfdbc89e97fcc40b8101ad1c40
Noe Prado <pranazul@gmail.com>
2022-03-19 17:28:58 -0600

    funciones de activacion

--- activations.py
+++ activations.py
@@ -21,3 +21,23 @@ def sigmoid(z, derivative=False):
         da = a * (1 - a)
         return a, da
     return a
+
+
+def relu(z, derivative=False):
+    a = z * (z >= 0)
+    if derivative:
+        da = np.array(z >= 0, dtype=float)
+        return a, da
+    return a
+
+def activate(function_name):
+    if function_name == 'linear':
+        return linear
+    elif function_name == 'tanh':
+        return tanh
+    elif function_name == 'sigmoid':
+        return sigmoid
+    elif function_name == 'relu':
+        return relu
+    else:
+        raise ValueError('function_name unknown')
